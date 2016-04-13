da50f6d6a9da50317c991ad429154da45313f9d9
LouisChang <changsidi@gmail.com>
2016-04-13 12:27:53 -0400

    change

--- analysis/check_user.py
+++ analysis/check_user.py
@@ -3,26 +3,6 @@
 import argparse
 import simplejson as json
 
-"""
-{
-    'type': 'user',
-    'user_id': (encrypted user id),
-    'name': (first name),
-    'review_count': (review count),
-    'average_stars': (floating point average, like 4.31),
-    'votes': {(vote type): (count)},
-    'friends': [(friend user_ids)],
-    'elite': [(years_elite)],
-    'yelping_since': (date, formatted like '2012-03'),
-    'compliments': {
-        (compliment_type): (num_compliments_of_this_type),
-        ...
-    },
-    'fans': (num_fans),
-}
-
-"""
-
 def check_user_key(inputFile):
     users = set()
     all_users = set()
